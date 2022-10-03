bba98e390606623e487b9b3785d47beed0f3cdd4
FireSquid <jdeiss06@gmail.com>
2022-10-02 23:04:45 -0500

    add state machine

---
+++ src/shared/state_machine/state.gd
@@ -0,0 +1,63 @@
+extends Node
+class_name State
+
+
+signal entered()  # emitted when this state is entered into
+signal exitted()  # emitted when this state is exited
+var active_signals := []  # list of active signal objects
+
+# virtual method for chaning the state
+# called by the state machine when another state requests to change into this state. If it returns false, the change is aborted.
+func _can_change() -> bool:
+	return true
+
+
+# runs whenever this state is entered into
+func _enter(args := []) -> void:
+	pass
+
+
+# runs whenever this state is exited. Runs before the previous state is entered.
+func _exit(args := []) -> void:
+	pass
+
+
+# executed whenever the process_logic() method on the state machine is called
+func _logic(delta: float = -1.0) -> void:
+	pass
+
+
+# executted whenever the process_supplementary_logic() method on the state_machine is called
+func _supplementary_logic() -> void:
+	pass
+
+
+# same as _input(), but is only called when active
+func _active_input(event: InputEvent) -> void:
+	pass
+
+
+# adds a signal that is activated and deactivated based on when this state is active
+func _add_active_signal(signaler: Object, signal_name: String, method: Callable):
+	var active_signal: ActiveSignal = ActiveSignal.new(signaler, signal_name, method)
+	connect('entered', Callable(active_signal, '_on_State_entered'))
+	connect('exited', Callable(active_signal, '_on_State_exited'))
+
+
+class ActiveSignal:
+	var signaler: Object
+	var signal_name: String
+	var method: Callable
+	
+	func _init(_signaler: Object, _signal_name: String, _method: Callable):
+		#setup names
+		signaler = _signaler
+		signal_name = _signal_name
+		method = _method
+	
+	func _on_State_entered():
+		return signaler.connect(signal_name, method)
+	
+	
+	func _on_State_exited():
+		return signaler.disconnect(signal_name, method)
---
+++ src/shared/state_machine/state_machine.gd
@@ -0,0 +1,75 @@
+extends Node
+class_name StateMachine
+
+
+var state_history := []  # the history of states
+var possible_states := []  # the names of every possible state
+var selected_state: State  # the currently selected state
+@export_node_path(State) var starting_state  # the state the machine starts on
+@export var output_changes: bool = false  # whether the state machine outputs its changes or not
+@export_range(0, 50) var max_state_history_length: int = 20  # the maximum length the state history can be
+
+
+signal state_changed(old_state, new_state)  # emitted whenever the state is changed
+signal logic_finished()  # emitted whenever the game logic is finished processing
+signal supplementary_logic_finished()  # emitted whenever the supplementary logic is finished processing
+
+
+func _ready() -> void:
+	# get the possible states
+	for child in get_children():
+		child = child as Node
+		possible_states.append(child.get_name())
+	
+	# start the initial state
+	change_state(starting_state)
+
+
+func change_state(new_state: String, enter_args := [], exit_args := []) -> bool:
+	# make sure the new state is a possible state
+	assert(new_state in possible_states)
+	
+	var old_name = 'none'
+	var new_name = 'none'
+	
+	# exit out of the old state
+	if get_state(new_state)._can_transition():
+		if selected_state:
+			# run the old state's exit function
+			selected_state._exit()
+			selected_state.emit_signal("exited")
+			
+			# edit the state history
+			state_history.insert(0, selected_state.name)
+			if len(state_history) > max_state_history_length:
+					var _end_state = state_history.pop_back()
+			
+			old_name = selected_state.name
+		
+		# enter into the new state
+		selected_state = get_state(new_state)
+		selected_state._enter(enter_args)
+		new_name = selected_state.name
+		selected_state.emit_signal("entered")
+		
+		# output debug
+		if output_changes:
+			print('Switched from ' + old_name + ' to ' + new_name)
+		
+		# emit signals
+		emit_signal('state_changed', old_name, new_name)
+		
+		return true
+		
+	if output_changes:
+		print('State Change Failed')
+	return false
+
+
+func process_logc(delta) -> void:
+	
+	emit_signal('logic_finished')
+
+
+func get_state(name) -> State:
+	return get_node(name)
