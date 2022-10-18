d4b8ca4ac6e0ca655fee8f239a83e5577e8af4e2
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2022-10-18 14:19:29 -0500

    Fix #34

--- src/level_building/placeables/interactables/interactable.gd
+++ src/level_building/placeables/interactables/interactable.gd
@@ -4,12 +4,20 @@ class_name Interactable
 
 signal interacted()
 var hovered := false  # whether the player is hovering over the interactable or not
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-# TODO: track the hovered variable
-# TODO: have the key object fade in and out based on hovered
-# TODO: add outline?
 
 func _physics_process(delta) -> void:
 	if Input.is_action_just_pressed("interact") and hovered:
 		Console.output("Interacted with!")
 		emit_signal("intercted")
+
+
+func _on_interactable_body_entered(_body):
+	hovered = true
+	animation_player.play("fade")
+
+
+func _on_interactable_body_exited(body):
+	hovered = false
+	animation_player.play_backwards("fade")
