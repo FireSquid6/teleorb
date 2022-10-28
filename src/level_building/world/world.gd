da48062620bb0c4367980451f4fc5289caec2e2a
firesquid6 <jdeiss06@gmail.com>
2022-10-27 21:09:37 -0500

    add prototype light sprite system

--- src/level_building/world/world.gd
+++ src/level_building/world/world.gd
@@ -5,6 +5,7 @@ class_name World
 @onready var shape_containers: Node2D = $Areas
 @onready var level: Level = get_parent()
 @onready var camera: Camera2D = $"../Player/Camera"
+@onready var tilemap: TileMap = $Map
 var player: Player
 var camera_shapes: Array[Rect2] = []  # an array of shape 2D's used to define different rooms for the camera
 var light_textures: Array[Texture] = []  # an array of textures for lighting
@@ -30,14 +31,20 @@ func _ready():
 	
 	# TODO: generate light textures from the camera areas
 	Console.output('[color=green]Generating light textures:[/color]')
-	Console.output('[color=red]Feature not implemented yet[/color]')
+	for shape in camera_shapes:
+		shape = shape as Rect2
+		var image := create_light_texture(shape, tilemap)
+		
+		# remember to remove these later
+		image.save_png("user://image.png")
+		OS.shell_open(ProjectSettings.globalize_path("user://"))
+		break
 	Console.output('')
 	
 	shape_containers.queue_free()
 
 
 func _process(delta):
-	
 	if !active_rectangle.has_point(player.position):
 		for rect in camera_shapes:
 			rect = rect as Rect2
@@ -64,3 +71,49 @@ func collision_shape_2d_to_rect2(collision_shape: CollisionShape2D) -> Rect2:
 		end.x = (collision_shape.position + half_size).x
 		end.y = (collision_shape.position + half_size).y
 	return Rect2(pos, end - pos)
+
+
+func create_light_texture(rect: Rect2, tilemap: TileMap, layer: int = 0, blend: float = 0.125) -> Image:
+	var lightmap: Image = Image.new();
+	lightmap.create(rect.size.x, rect.size.y, false, Image.FORMAT_RGB8);
+	lightmap.fill(Color.BLACK)
+	
+	var tile_size: Vector2i = tilemap.tile_set.tile_size;
+	var start_cell: Vector2i = Vector2i(rect.position) / tile_size;
+	var cells: Vector2i = Vector2i(rect.size) / tile_size;
+	
+	# iterates through the rows and columns of the tilemap
+	for column in cells.x:
+		for row in cells.y:
+			var x_cell: int = start_cell.x + column
+			var y_cell: int = start_cell.y + row
+			
+			var x: int = x_cell * tile_size.x
+			var y: int = y_cell * tile_size.y
+			
+			if tilemap.get_cell_tile_data(layer, Vector2i(x_cell, y_cell)) != null:
+				var fill_rect = Rect2(Vector2(x, y), tile_size)
+				lightmap.fill_rect(fill_rect, Color.WHITE)
+	
+	# blend the edges of the lightmap out
+#	var value: float = 1.0
+#	var black := Color.BLACK
+#	while value > 0.0:
+#		var color = Color(value, value, value)
+#		for x in range(rect.size.x):
+#			for y in range(rect.size.y):
+#				# I should find a better way to do this
+#				if (lightmap.get_pixel(x - 1, y) != black
+#					or lightmap.get_pixel(x + 1, y) != black
+#					or lightmap.get_pixel(x, y + 1) != black
+#					or lightmap.get_pixel(x, y - 1) != black
+#					or lightmap.get_pixel(x - 1, y - 1) != black
+#					or lightmap.get_pixel(x + 1, y + 1) != black
+#					or lightmap.get_pixel(x + 1, y - 1) != black
+#					or lightmap.get_pixel(x - 1, y + 1) != black):
+#						lightmap.set_pixel(x, y, color)
+#
+#		value -= blend
+	
+	
+	return lightmap
