5b99598e5f6867fdaf3b4fd7ae5c602fa6edd759
firesquid6 <jdeiss06@gmail.com>
2024-03-25 17:47:52 -0500

    add a default.nix

---
+++ README.md
@@ -0,0 +1,8 @@
+# Teleorb
+Teleorb is a multiplayer speedrun based platformer. The tech stack is:
+
+- Client side game built in Godot 4 `/game`
+- Server built in Godot 4 `/server`
+- Authentication and database using bun, 
+
+All deployments are done through [fly.io](https://fly.io)
---
+++ default.nix
@@ -0,0 +1,23 @@
+# Help! I'm a nix user and it isn't working!
+#
+# try running these commands:
+# sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+# sudo nix-channel --update
+# then run nix-shell again
+
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
+{ nixpkgs ? import <nixpkgs> { } }:
+with nixpkgs; mkShell {
+  DOCKER_BUILDKIT = "1";
+  buildInputs = [
+    unstable.bun
+    nodejs_20
+    flyctl
+    libgcc
+
+    # You'll also need Godot 4 installed if you're making the server or the game
+    # This isn't specified in the default.nix because Godot is a desktop app
+  ];
+}
