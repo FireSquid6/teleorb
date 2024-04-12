dca56f9c5b26dff8e5db36cf8361c380e995831d
firesquid6 <jdeiss06@gmail.com>
2024-04-11 22:23:18 -0500

    start on level assembly

--- game/level/level.gd
+++ game/level/level.gd
@@ -4,6 +4,8 @@ class_name Level
 @export var entities: Node2D
 @export var _orb_scene: PackedScene
 @export var _player_scene: PackedScene
+@export var segments: Array[Segment]
+
 
 func _ready():
 	World.curret_level = self
@@ -18,6 +20,9 @@ func _ready():
 	
 	if not Server.is_dedicated_server:
 		_add_player(1)
+	
+	var section = Section.new(Segment.BIOMES.CAVE, segments)
+	section.spawn_in(self, $Startpoint.position)
 
 
 @rpc("any_peer", "call_local", "reliable")
@@ -62,3 +67,62 @@ func _remove_player(id: int):
 	if not entities.has_node(str(id)):
 		return
 	entities.get_node(str(id)).queue_free()
+
+class Section:
+	var biome: Segment.BIOMES
+	var segments: Array[Segment]
+	
+	
+	func _init(b: Segment.BIOMES, s: Array[Segment]):
+		biome = b
+		segments = s
+	
+	func spawn_in(level: Level, start_position: Vector2):
+		randomize()
+		print(segments)
+		segments.shuffle()
+		var to_free: Array[Node] = []
+		var next_start := start_position
+		
+		# we need to make sure all segments have the same biome
+		for segment in segments:
+			assert(segment.biome == biome) 
+		
+		var transition_scene: PackedScene = Segment.transitions[segments[0].biome]
+		var nodes: Array[Node2D] = []
+		
+		for i in range(len(segments) * 2 - 1):
+			if i % 2 == 0:
+				var index = floor(i / 2)
+				var segment: Segment = segments[index]
+				nodes.append(segment.scene.instantiate())
+			else:
+				nodes.append(transition_scene.instantiate())
+		
+		for node in nodes:
+			level.add_child(node)
+			var startpoint_node: Node2D = null
+			var endpoint_node: Node2D = null
+			
+			for c in node.get_children():
+				if c.name == "Startpoint":
+					startpoint_node = c
+				if c.name == "Endpoint":
+					endpoint_node = c
+			
+			# if these are failing, a segment is improperly configured
+			assert(endpoint_node != null)
+			assert(startpoint_node != null)
+			
+			# spawn the segment and move it to the proper location
+			var difference = startpoint_node.position
+			node.position = next_start + difference
+			
+			next_start = node.position + node.position
+
+
+
+
+
+
+
--- game/segments/segment.gd
+++ game/segments/segment.gd
@@ -5,5 +5,10 @@ enum BIOMES {
 	CAVE
 }
 
+static var transitions: Dictionary = {
+	BIOMES.CAVE: preload("res://components/transitions/cave_transition.tscn")
+}
+
 @export var biome: BIOMES
 @export var scene: PackedScene
+@export var name: String
