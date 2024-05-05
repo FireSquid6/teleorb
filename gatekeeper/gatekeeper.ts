188431d0a11ce40d8b53faacaa5ef3df4813dbd7
firesquid6 <jdeiss06@gmail.com>
2024-05-05 16:10:56 -0500

    add client and server keys state

--- gatekeeper/gatekeeper.ts
+++ gatekeeper/gatekeeper.ts
@@ -9,6 +9,12 @@ interface GameServer {
   httpUrl?: string
 }
 
+interface Client {
+  id: string
+  gamemode: string
+  callbackUrl: string
+}
+
 
 function generateRandom(): string {
   return Math.random().toString(36).substring(2, 15)
@@ -19,8 +25,9 @@ const secret = "a secret. This will be changed when warden is implemented."
 
 export const app = new Elysia()
   .state("gameservers", [] as GameServer[])
-  .state("keys", new Map<string, string>())
-  .state("clients", [] as string[])
+  .state("serverKeys", new Map<string, string>())
+  .state("clientKeys", new Map<string, string>())
+  .state("client", [] as Client[])
   .get("/servers/:id", ({ set, params: { id }, store }): GameServer | undefined  => {
     for (const gameserver of store.gameservers) {
       if (gameserver.id === id) {
@@ -39,7 +46,7 @@ export const app = new Elysia()
     return store.gameservers
   })
   .delete("/servers/:id", async ({ store, set, body, params: {id} } ) => {
-    if (id !== store.keys.get(body.key)) {
+    if (id !== store.serverKeys.get(body.key)) {
       set.status = 401
       await new Promise((resolve) => setTimeout(resolve, 5000))
     }
@@ -71,7 +78,7 @@ export const app = new Elysia()
       httpUrl: body.httpUrl,
     })
 
-    store.keys.set(key, key)
+    store.serverKeys.set(key, key)
 
 
     return {
@@ -93,7 +100,22 @@ export const app = new Elysia()
 
   })
   .delete("/clients/:id", async ({ store, set, body, params: {id} }) => {
+    if (id === store.clientKeys.get(body.key)) {
+      set.status = 204
+      store.clientKeys.delete(body.key)
+      store.client = store.client.filter((client) => client.id !== id)
+    } else {
+      set.status = 401
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    }
 
+  }, {
+    body: t.Object({
+      key: t.String(),
+    }),
+    params: t.Object({
+      id: t.String()
+    })
   })
 
 export type App = typeof app
